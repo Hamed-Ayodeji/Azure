c0dabf004afdbca206c3bcb2b898a404e4c92bf7
Ayodeji Hamed <ayodejihamed@gmail.com>
2024-02-20 21:10:35 +0100

    azure tutorial

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    azurerm       = {
+      source      = "hashicorp/azurerm"
+      version     = "3.92.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "b0019fad-dab9-44d7-8ce9-d2943a830c18"
+  client_id       = "cc9d3cad-37db-484c-9a9f-d568298f4dd6"
+  client_secret   = "MPR8Q~7rFu~gtcxtcBZu1ojgaW6uYNSjLgIDxayz"
+  tenant_id       = "4468283d-61db-4cfb-8c2d-7d87f3551df2"
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "qurtstorageaccount2024"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "mycontainer"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "blob"
+  depends_on = [ azurerm_storage_account.storage ]
+}
+
+resource "azurerm_storage_blob" "blob" {
+  name                   = "sample.txt"
+  storage_account_name   = azurerm_storage_account.storage.name
+  storage_container_name = azurerm_storage_container.container.name
+  type                   = "Block"
+  source                 = "sample.txt"
+  depends_on = [ azurerm_storage_container.container ]
+}
+
